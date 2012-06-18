eaf3fbc0791f8eb8e8fa4fa7138fb6f517ba461c
Elliot <elliot@oxygenproductions.com>
2012-06-18 16:25:45 -0700

    email

--- routes/blog/index.js
+++ routes/blog/index.js
@@ -1,8 +1,20 @@
 
-var Posts = require('../../models/post.js');
+var Posts = require('../../models/post.js')
+  , url = require('url')
+  , lib = {
+      common      : require('../../lib/common.js')
+  };
 
 exports.index = function(req, res) {
-  Posts.find({}).where('status', 1).sort('created', -1).exec(function(err, posts) {
+  var query = Posts.find({}).where('status', 1).sort('created', -1);
+  var qs = url.parse(req.url, true).query;
+  if (qs['tag']) {
+    query.where('tags').in([ qs['tag'] ])
+  }
+  query.exec(function(err, posts) {
+    if (lib.common.e(posts)) {
+      posts = 'none';
+    }
     Posts.find({}).where('status', 1).where('featured', 1).sort('created', -1).exec(function(err, featured) {
       res.render('blog/index', { title: 'Blog', posts: posts, featured: featured });
     });

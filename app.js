d28b5d562865ff258acf576e37f0f7461c27b74d
Elliot <elliot@oxygenproductions.com>
2012-06-25 19:18:36 -0700

    tmp admin access removal

--- app.js
+++ app.js
@@ -36,12 +36,17 @@ app.configure(function(){
 });
 
 var isAdmin = function(req, res, next) {
+  
+  next(); //tmp while fixing login on server
+  
+  /*
   if (req.loggedIn === true) {
     next();
   }
   else {
     res.redirect('/login');
   }
+  */
 }
 
 app.configure('development', function(){
